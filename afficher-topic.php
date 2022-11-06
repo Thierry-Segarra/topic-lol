4970cf03d78c8a2d946c5048187280532eb5d82c
Thierry-Segarra <Thierrysegarra130140@gmail.com>
2022-11-06 18:17:06 +0100

    add scroll js in one page

--- afficher-topic.php
+++ afficher-topic.php
@@ -80,30 +80,4 @@
     </a>
 </body>
 </html>
-<script>
-
-window.onscroll = function(ev) {
-    
-        if (window.scrollY > 100) {
-            document.getElementById("navbar").style.background = "black";
-            document.getElementById("scrollUp").style.right = "10px";
-            
-            var li = document.querySelectorAll(".nav-links li #navlinks");
-
-            for(var i = 0; li[i]; i++){       
-            li[i].style.color = "white";
-            }
-
-        }
-        else{
-            document.getElementById("navbar").style.background = "none";
-            document.getElementById("scrollUp").style.right = "-100px";
-            var li = document.querySelectorAll(".nav-links li #navlinks");
-
-            for(var i = 0; li[i]; i++){       
-            li[i].style.color = "black";
-            }
-        }
-};
-
-</script>
+<?php include('scroll.php') ?>
