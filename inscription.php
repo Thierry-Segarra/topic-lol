ea93ae6f4aaa724a617cc6be466562f1e2906dae
Erwan Bauer <97156314+Tetdoss13@users.noreply.github.com>
2023-02-26 19:38:29 +0100

    test V1.1

--- inscription.php
+++ inscription.php
@@ -7,12 +7,12 @@
 
 <body>
     <!--<div class="cursor"></div> -->
-    <img class="fond" src="img/background-LOL.jpg"/>
-    <?php include("header.php");?>
+    <img class="fond" src="img/darkred.jpg"/>
+    <?php include("header2.php");?>
     <br>
     <div id="container-connexion">            
         <form action="Fonction-php&JS/inscri.php" method="POST">
-            <h1>Inscrivez-vous</h1>
+            <h1>Inscription</h1>
             <br>
             <label class="form_label"><b>Nom d'utilisateur</b></label>
             <input type="text" class="form_input" placeholder="Entrer le nom d'utilisateur" name="username" required>
@@ -43,7 +43,7 @@
             
         </form>
     </div>
-    <footer><?php include('footer.php'); ?></footer>
+    <footer><?php include('footer2.php'); ?></footer>
 </body>
 
 </html>
